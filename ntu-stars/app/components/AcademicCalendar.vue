bc38130e1670f30f7f15ee2c41319d70866b00a1
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-01-08 15:09:40 +1100

    Edit calendar and profile card display

--- ntu-stars/app/components/AcademicCalendar.vue
+++ ntu-stars/app/components/AcademicCalendar.vue
@@ -2,16 +2,17 @@
     <ClientOnly>
         <UCalendar
             readonly
-            size="md"
             month-controls
             !fixed-weeks
+            class="w-70 aspect-square"
         >
             <template #day="{ day }">
                 <div
                     class="w-full h-full flex items-center justify-center rounded-full"
-                    :class="ringToday(day)"
                 >
-                    <span :class="`text-${getColorFromDay(day)}`">{{ day.day }}</span>
+                    <span :class="isWeekend(day) ? 'text-red-600' : 'text-black' + isToday(day) ? ' border-2 border-imperial-600' : ''">
+                        {{ day.day }}
+                    </span>
                 </div>
             </template>
         </UCalendar>
@@ -20,25 +21,11 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue';
-import { CalendarDate, today, getDayOfWeek, type DateValue } from '@internationalized/date';
+import { today, getDayOfWeek, type DateValue } from '@internationalized/date';
 
 const todayDate = ref<DateValue>(today('Asia/Singapore'));
 
-function getColorFromDay(date: DateValue): string {
-    const isWeekend: boolean = getDayOfWeek(date, 'en-US') % 6 === 0;
+const isWeekend = (date: DateValue): boolean => getDayOfWeek(date, 'en-US') % 6 === 0;
 
-    if (isWeekend) {
-        return 'red-500';
-    }
-
-    return 'black';
-}
-
-function ringToday(date: DateValue): string {
-    if (date === todayDate.value) {
-        return 'ring-2 ring-imperial-600';
-    }
-
-    return '';
-}
+const isToday = (date: DateValue): boolean => date === todayDate.value;
 </script>
--- ntu-stars/app/components/ProfileCard.vue
+++ ntu-stars/app/components/ProfileCard.vue
@@ -1,33 +1,34 @@
 <template>
-    <UCard class="w-450 aspect-3/2 border-2 border-gold-700 bg-cardinal-200 shadow-xl shadow-slate-600">
+    <UCard class="w-90 aspect-3/2 border-2 border-gold-700 bg-cardinal-200 shadow-xl shadow-slate-600">
         <template #header>
-            <h3 class="text-primary-500 text-2xl">
+            <h3 class="text-primary-500 text-xl font-semibold">
                 Student Name
             </h3>
         </template>
 
-        <div class="flex flex-row">
+        <div class="flex flex-row justify-center gap-6 items-baseline-center">
             <div class="flex flex-col justify-center items-center">
                 <img
                     src="/profile_avatar_placeholder.png"
                     alt="student image"
+                    class="h-25 aspect-2/3"
                 >
             </div>
-            <div class="flex flex-col justify-evenly items-center space-y-auto">
+            <div class="flex flex-col justify-evenly items-start space-y-auto">
                 <p
                     class="text-xs md:text-sm xl:text-2xl font-semibold"
                 >
-                    <span>ID:</span> Student ID
+                    <span class="text-gold-600 italic">ID:</span> Student ID
                 </p>
                 <p
                     class="text-xs md:text-sm xl:text-2xl font-semibold"
                 >
-                    <span>Program:</span> Student Program
+                    <span class="text-gold-600 italic">Program:</span> Student Program
                 </p>
                 <p
                     class="text-xs md:text-sm xl:text-2xl font-semibold"
                 >
-                    <span>School:</span> Student School
+                    <span class="text-gold-600 italic">School:</span> Student School
                 </p>
             </div>
         </div>
