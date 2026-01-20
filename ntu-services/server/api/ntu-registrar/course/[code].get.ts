493f6c75a55c88597be4fff8369350bbf6341607
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-01-20 17:25:16 +1100

    Rewrite error message.

--- ntu-services/server/api/ntu-registrar/course/[code].get.ts
+++ ntu-services/server/api/ntu-registrar/course/[code].get.ts
@@ -1,13 +1,23 @@
 import courseRest from '../../../resource/courseRest';
 
 export default defineEventHandler(async (e) => {
+
+    const query = getQuery<{ fields?: string }>(e);
+    const fields: string[] | undefined = query?.fields?.split(',');
     const code: string | undefined = getRouterParam(e, 'code');
 
-    const course = courseRest.retrieveCourse(code); 
+    if (!code) {
+        throw createError({
+            statusCode: 400,
+            statusMessage: '400 Bad Request: please include the course code'
+        })
+    }
+
+    const course = courseRest.retrieveCourse({option: fields, code: code}); 
     if (!course) {
         throw createError({
             statusCode: 404,
-            statusMessage: `Course with code: ${code} not found`
+            statusMessage: `404 Not Found: Course with code ${code} does not exist.`
         });
     }
 
