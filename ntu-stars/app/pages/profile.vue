33b506637e7496278803e0a6e7e7545941f2b0f5
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-12 11:23:24 +1100

    Define web slots

--- ntu-stars/app/pages/profile.vue
+++ ntu-stars/app/pages/profile.vue
@@ -3,12 +3,14 @@
         <NavBar />
         <TitleSection :title="'Student Profile'" />
         <UPageSection>
-            <div class="flex flex-row justify-between lg:px-20 xl:px-40">
-                <ProfileCard />
-                <AcademicCalendar />
-            </div>
+            <template #description>
+                <div class="flex flex-row justify-between py-0 lg:px-20 xl:px-40">
+                    <ProfileCard />
+                    <AcademicCalendar />
+                </div>
+            </template>
         </UPageSection>
-        <UPageSection></UPageSection>
+        <UPageSection />
         <div class="mb-20">
             <br>
         </div>
