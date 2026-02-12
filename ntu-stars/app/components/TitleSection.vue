9fa0cebe4e59712871b3d7a8222d006b7e99d5a3
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-12 17:12:29 +1100

    Change title size.

--- ntu-stars/app/components/TitleSection.vue
+++ ntu-stars/app/components/TitleSection.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-row h-15 items-center">
         <div class="w-5/6">
-            <h1 class="text-3xl text-imperial-700 text-underline font-bold text-center">
+            <h1 class="text-4xl text-imperial-700 text-underline font-bold text-center">
                 {{ props.title }}
             </h1>
         </div>
