2a668ee21fa9e39b921eb48c218dff2fb100cc79
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-12 11:23:52 +1100

    Add pageBody and pageSection.

--- ntu-stars/app/pages/addDrop.vue
+++ ntu-stars/app/pages/addDrop.vue
@@ -2,11 +2,20 @@
     <UPage>
         <NavBar />
         <TitleSection :title="'Add/Drop Course'" />
-        <UPageBody class="flex-col">
-            <h2 class="text-cardinal-600 font-semibold text-2xl">
-                Enrol in Course
-            </h2>
-            <CourseTable />
+        <UPageBody>
+            <UPageSection class="py-0">
+                <template #header>
+                    <h2 class="text-cardinal-600 font-semibold text-2xl">
+                        Enrol in Course
+                    </h2>
+                </template>
+                <template #body>
+                    <CourseTable />
+                </template>
+            </UPageSection>
         </UPageBody>
+        <div class="mb-20">
+            <br>
+        </div>
     </UPage>
 </template>
