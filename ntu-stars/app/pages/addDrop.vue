689b165f707cfc763f3f618938a872513f0145e3
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-17 20:38:38 +1100

    Implement data fetching on page.

--- ntu-stars/app/pages/addDrop.vue
+++ ntu-stars/app/pages/addDrop.vue
@@ -10,8 +10,8 @@
                         Enrol in Course
                     </h2>
                 </template>
-                <template #body>
-                    <CourseTable />
+                <template #default>
+                    <CourseTable :courses="courses" :pending="pending" />
                 </template>
             </UPageSection>
         </UPageBody>
@@ -19,8 +19,38 @@
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, computed } from 'vue';
+import { authStore } from '../../store/authStore';
+import type { coursePacket } from '../../../ntu-services/server/resource/courseRest';
+import { Course } from '../../../shared/course';
+
 definePageMeta({
     layout: false
-})
+});
+
+const auth = authStore();
+const rawCourses = ref<coursePacket[]>([]);
+const pending = ref<boolean>(true);
+
+onMounted(async () => {
+    try {
+        const data = await $fetch<coursePacket[]>('/api/ntu-registrar/course', {
+            query: {
+                fields: 'cos_code,cos_title,cos_au'
+            },
+            headers: {
+                Authorization: `Bearer ${auth.getToken()}`
+            }
+        });
+        rawCourses.value = data;
+    } catch (error) {
+        console.error('Error fetching courses: ', error);
+    } finally {
+        pending.value = false;
+    }
+});
+
+const courses = computed<Course[]>(() => (rawCourses.value ?? []).map((cp: coursePacket) => new Course(cp.cos_code, cp.cos_title, cp.cos_au)));
+
 </script>
 
