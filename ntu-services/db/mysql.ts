b67aa14a9385c5fe5e22b1c39c2275e953709c82
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-17 12:07:28 +1100

    Add getter for pool.

--- ntu-services/db/mysql.ts
+++ ntu-services/db/mysql.ts
@@ -1,14 +1,25 @@
 import mysql, { type Pool } from 'mysql2/promise';
 
-const config = useRuntimeConfig();
+let pool: Pool;
 
-const pool: Pool = mysql.createPool({
-      host: config.mysqlHost || 'localhost',
-      user: config.mysqlUser || 'nanyang60',
-      password: config.mysqlPassword || 'NTUSingapore+60',
-      database: config.mysqlDBName || 'nanyang_stars',
-      waitForConnections: true,
-      connectionLimit: 20,
-});
+export const getPool = (): Pool => {
+      if (!pool) {
+            const config = useRuntimeConfig();
+            pool = mysql.createPool({
+                  host: config.mysqlHost || 'localhost',
+                  user: config.mysqlUser || 'nanyang60',
+                  password: config.mysqlPassword || 'NTUSingapore+60',
+                  database: config.mysqlDBName || 'nanyang_stars',
+                  waitForConnections: true,
+                  connectionLimit: 20,
+            });
+      }
+      return pool;
+};
 
-export default pool;
+export default {
+      execute: async <T>(sql: string, params?: any[]): Promise<[T, any]> => {
+            const p = getPool();
+            return p.execute<any>(sql, params) as Promise<[T, any]>;
+      }
+};
